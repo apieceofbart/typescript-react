739a5270af0003fb5753795b5adaa5b381fe0e8d
apieceofbart <bartek.kozera@gmail.com>
2018-09-09 20:32:11 +0200

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,17 @@ module.exports = function (env) {
       path: `${__dirname}/dist`,
       filename: 'main.bundle.js'
     },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        }
+      ]
+    },
     plugins: [new HtmlWebpackPlugin({
       template: 'index.html'
     })]
